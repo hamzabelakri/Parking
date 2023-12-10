cac8d3dc33a8a3bba6c07c11dd1a820fd3605403
hamzabelakri <94742741+hamzabelakri@users.noreply.github.com>
2023-12-10 18:30:34 +0100

    Delete FastApi directory

--- FastApi/config/database.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client= MongoClient("mongodb+srv://Hamza:mongodb@cluster0.4nppq.mongodb.net/?retryWrites=true&w=majority")
-
-db=client.parking
-
-collection_name = db["client.parking"]
